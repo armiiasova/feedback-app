e1b0df298bad787dba565b532dadf28d0b82e1ba
Cholpon Zhanybek uulu <armiiasova.pl@gmail.com>
2023-05-04 20:23:35 +0200

    All parts need to be updated and added

---
+++ feedback-app/src/App.js
@@ -0,0 +1,17 @@
+function App() {
+    return (
+        <div className='container'>
+            <h1>My App</h1>
+        </div>
+    )
+} 
+
+
+
+
+Header.defaultProps = {
+    text: 'Feedback UI',
+
+}
+
+export default App
---
+++ feedback-app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header({text}) {
+    return (
+        <>
+        <Header text="Hello World" />
+            <div className="container">
+            <h2>{text}</h2>
+            </div>
+        </>
+    )
+ }
+
+export default Header
